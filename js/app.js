2bdf6d9ea7cb364f4868c2a204fa4a3de921052e
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-03-01 16:54:45 +0600

    add phone img and phone name

--- js/app.js
+++ js/app.js
@@ -7,15 +7,23 @@ const loadPhones = async () => {
 }
 
 const displyPhones = phones => {
-    const phonesContainer = document.getElementById('phone-container');
+    const phonesContainer = document.getElementById('phones-container');
     phones.forEach(phone => {
         // create element 
         const phoneDiv = document.createElement('div');
         // add class list dynamically
         phoneDiv.classList.add('col');
         phoneDiv.innerHTML = `
-        
+        <div class="card">
+            <img src="${phone.image}" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
         `;
+        phonesContainer.appendChild(phoneDiv);
     })
 }
 
