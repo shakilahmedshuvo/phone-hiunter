fe136a2649fbd96eacde376c4b94709857f8fab9
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-03-01 14:01:54 +0600

    add class or card form bs and some html

--- js/app.js
+++ js/app.js
@@ -3,7 +3,11 @@ const loadPhones = async () => {
     const url = 'https://openapi.programming-hero.com/api/phones?search=iphone'
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
+    displyPhones(data.data);
+}
+
+const displyPhones = phones => {
+    console.log(phones);
 }
 
 // call the function
